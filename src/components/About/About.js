f44cdafffd0cb74beb1cea7d16d54fb80a5c0090
venkatk-git <venkatkumar.profiles@gmail.com>
2024-07-09 11:05:16 +0530

    Updated To About Me Grid

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,24 +3,34 @@ import styles from "./about.module.css";
 
 function About() {
   return (
-    <div className={styles.wrapper} id="about">
-      <div className={styles.main}>
-        <img src=""></img>
-        <p>
-          Hello! I&apos;m <span className={styles.italic}>Venkat Kumar</span>,
-          current III year student at Bannari Amman Institute of Technology. I
-          create websites that solve real-world problems, transforming ideas
-          into interactive digital experiences. My portfolio showcases projects
-          addressing everyday challenges. Committed to continuous learning, I
-          stay updated with modern web technologies and deployment practices. I
-          thrive in collaborative environments, tackling challenges with
-          adaptability and creativity. I aspire to contribute to impactful tech
-          projects, making a positive difference with my web development
-          expertise. Feel free to explore my portfolio and reach out to connect!
-        </p>
-      </div>
-      <div className={styles.right}>
-        <h1>😃</h1>
+    <div className={styles.wrapper}>
+      <h1 className={styles.sectionTitle}>About Me</h1>
+      <div className={styles.gridWrapper} id="about">
+        <div className={styles.main}>
+          <img
+            src="https://raw.githubusercontent.com/venkatk-git/portfolio/main/public/assests/profile_placeholder.jpg"
+            className={styles.profileImg}
+          ></img>
+          <p>
+            Hello! I&apos;m <span className={styles.italic}>Venkat Kumar</span>,
+            current III year student at Bannari Amman Institute of Technology. I
+            create websites that solve real-world problems, transforming ideas
+            into interactive digital experiences.
+          </p>
+        </div>
+        <div className={styles.techStack}>
+          <h1>Stack</h1>
+        </div>
+        <div className={styles.languages}>
+          <h1>List of languages</h1>
+        </div>
+        <div className={styles.objective}>
+          <p>
+            &quot;I aspire to contribute to impactful tech projects, making a
+            positive difference with my web development expertise. Feel free to
+            explore my portfolio and reach out to connect!&quot;
+          </p>
+        </div>
       </div>
     </div>
   );
