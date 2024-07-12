dea677c80ae26af8c8226a29269cd56ccb386aee
venkatk-git <venkatkumar.profiles@gmail.com>
2024-07-12 14:57:13 +0530

    aceternity-intergration branch test commit

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,7 +6,7 @@ function Hero() {
     <>
       <div className={styles.wrapper}>
         <div className={styles.noteWrapper}>
-          <span>Last Commit on 5th July 2024</span>
+          <span>Last Commit on 12th July 2024</span>
           <span>🟢</span>
         </div>
         <h1 className={styles.heading}>
