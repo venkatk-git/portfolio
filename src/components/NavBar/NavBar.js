643d7b4539e11b374c68ccb793b5fe3228803e15
venkatk-git <venkatkumar.profiles@gmail.com>
2024-07-21 10:12:51 +0530

    Mobile Nav 01

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,12 +1,51 @@
+"use client";
 import React from "react";
+
+import { IoMdClose } from "react-icons/io";
 
 import styles from "./navBar.module.css";
 
-function NavBar() {
+const MobileNavList = ({ mobileNavState }) => {
   return (
-    <div className={styles.wrapper}>
+    mobileNavState && (
+      <ul className={styles.mobileNavList}>
+        <a className={styles.navItem} href="#projects">
+          Projects
+        </a>
+        <a className={styles.navItem} href="#about">
+          About
+        </a>
+        <a className={styles.navItem} href="#contact">
+          Contact
+        </a>
+      </ul>
+    )
+  );
+};
+
+function NavBar() {
+  const [mobileNavState, setMobileNavState] = React.useState(false);
+
+  const NAV_SYMBOL = mobileNavState ? "X" : "V";
+
+  const handleMobileNavToggle = () => {
+    setMobileNavState(!mobileNavState);
+  };
+
+  return (
+    <div
+      className={`${styles.wrapper} ${
+        mobileNavState ? styles.mobileNavOpen : styles.mobileNavClose
+      }`}
+    >
       <div className={styles.blur} />
-      <ul className={styles.navList}>
+      <div className={styles.mobileNav}>
+        <button className={styles.navTrigger} onClick={handleMobileNavToggle}>
+          {NAV_SYMBOL}
+        </button>
+        <MobileNavList mobileNavState={mobileNavState} />
+      </div>
+      <ul className={styles.desktopNavList}>
         <a className={styles.logo} href="#">
           V
         </a>
