d31648e4793090f9a50f1d7a9eee7d8e959a35f7
venkatk-git <venkatkumar.profiles@gmail.com>
2024-07-21 09:22:03 +0530

    Added Queries in Contants

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,9 @@
 // Background Grid Size
 export const GRID_SIZE = 1290;
+
+//Queries
+export const QUERIES = {
+  mobileAndSmaller: "max-width(var(--mobile))",
+  tabletAndSmaller: "max-width(var(--tablet))",
+  laptopAndSmaller: "max-width(var(--laptop))",
+};
