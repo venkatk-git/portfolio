19315da85ee6ec8491d82d8003ed02a8c98ec11e
venkatk-git <venkatkumar.profiles@gmail.com>
2024-07-26 19:17:59 +0530

    Added infinite scroll to technology stacks

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,22 @@
+"use client";
 import React from "react";
 import styles from "./about.module.css";
 
 import Image from "next/image";
+import { Icon } from "@iconify/react";
+
+function addAnimation() {
+  const scroller = document.querySelector(`.${styles.scroller}`);
+  scroller.setAttribute("data-animated", true);
+}
 
 function About() {
+  React.useEffect(() => {
+    if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+      addAnimation();
+    }
+  });
+
   return (
     <div className={styles.wrapper} id="about">
       <div className={styles.sectionHeading}>
@@ -29,10 +42,99 @@ function About() {
           </p>
         </div>
         <div className={`${styles.techStack} ${styles.gridItems}`}>
-          <h1>Stack</h1>
+          <div className={`${styles.techStackWrapper} ${styles.scroller}`}>
+            <div className={styles.scroller__inner}>
+              <Icon
+                icon="skill-icons:npm-light"
+                className={styles.iconifyIcon}
+              />
+              <Icon
+                icon="skill-icons:vite-light"
+                className={styles.iconifyIcon}
+              />
+              <Icon
+                icon="skill-icons:react-light"
+                className={styles.iconifyIcon}
+              />
+              <Icon
+                icon="skill-icons:nextjs-light"
+                className={styles.iconifyIcon}
+              />
+              <Icon
+                icon="skill-icons:nodejs-light"
+                className={styles.iconifyIcon}
+              />
+              <Icon icon="skill-icons:mongodb" className={styles.iconifyIcon} />
+              <Icon icon="skill-icons:postman" className={styles.iconifyIcon} />
+              <Icon icon="skill-icons:git" className={styles.iconifyIcon} />
+              <Icon
+                icon="skill-icons:github-light"
+                className={styles.iconifyIcon}
+              />
+              <Icon icon="skill-icons:docker" className={styles.iconifyIcon} />
+              <Icon
+                icon="skill-icons:npm-light"
+                className={`${styles.iconifyIcon} ${styles.animateOnly}`}
+              />
+              <Icon
+                icon="skill-icons:vite-light"
+                className={`${styles.iconifyIcon} ${styles.animateOnly}`}
+              />
+              <Icon
+                icon="skill-icons:react-light"
+                className={`${styles.iconifyIcon} ${styles.animateOnly}`}
+              />
+              <Icon
+                icon="skill-icons:nextjs-light"
+                className={`${styles.iconifyIcon} ${styles.animateOnly}`}
+              />
+              <Icon
+                icon="skill-icons:nodejs-light"
+                className={`${styles.iconifyIcon} ${styles.animateOnly}`}
+              />
+              <Icon
+                icon="skill-icons:mongodb"
+                className={`${styles.iconifyIcon} ${styles.animateOnly}`}
+              />
+              <Icon
+                icon="skill-icons:postman"
+                className={`${styles.iconifyIcon} ${styles.animateOnly}`}
+              />
+              <Icon
+                icon="skill-icons:git"
+                className={`${styles.iconifyIcon} ${styles.animateOnly}`}
+              />
+              <Icon
+                icon="skill-icons:github-light"
+                className={`${styles.iconifyIcon} ${styles.animateOnly}`}
+              />
+              <Icon
+                icon="skill-icons:docker"
+                className={`${styles.iconifyIcon} ${styles.animateOnly}`}
+              />
+            </div>
+          </div>
         </div>
         <div className={`${styles.languages} ${styles.gridItems}`}>
-          <h1>List of languages</h1>
+          <div className={styles.languagesWrapper}>
+            <Icon icon="skill-icons:c" className={styles.iconifyIcon} />
+            <Icon
+              icon="skill-icons:javascript"
+              className={styles.iconifyIcon}
+            />
+            <Icon
+              icon="skill-icons:java-light"
+              className={styles.iconifyIcon}
+            />
+            <Icon
+              icon="skill-icons:python-light"
+              className={styles.iconifyIcon}
+            />
+            <Icon
+              icon="skill-icons:typescript"
+              className={styles.iconifyIcon}
+            />
+          </div>
         </div>
         <div className={`${styles.objective} ${styles.gridItems}`}>
           <p>
