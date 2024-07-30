dc1976c2d8ae81d0e630c06ec6b9e860e118746f
venkatk-git <venkatkumar.profiles@gmail.com>
2024-07-30 22:41:27 +0530

    Form Data and autofill styles

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 
 import styles from "./contact.module.css";
@@ -51,6 +52,27 @@ function ContactTemp() {
 }
 
 function Contact() {
+  const [formData, setFormData] = React.useState({
+    who: "",
+    name: "",
+    email: "",
+    phone: "",
+    why: "",
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const newData = {
+      who: e.currentTarget.who.value,
+      name: e.currentTarget.name.value,
+      email: e.currentTarget.email.value,
+      phone: e.currentTarget.phone.value,
+      why: e.currentTarget.why.value,
+    };
+    setFormData(newData);
+  };
+
   return (
     <div className={styles.wrapper} id="contact">
       <div className={styles.contactHeadWrapper}>
@@ -63,25 +85,40 @@ function Contact() {
       <div className={styles.contactStack}>
         <div className={styles.handles}>Handles</div>
         <div className={styles.contactForm}>
-          <form className={styles.formWrapper}>
+          <form className={styles.formWrapper} onSubmit={onSubmit}>
             <h3 className={styles.formHeading}>Reach out.</h3>
             <div className={styles.formFieldWrapper}>
               <div className={styles.whoWrapper}>
                 <h3>Who are you?</h3>
-                <div className={styles.whoBtnWrapper}>
+                <fieldset group="who" className={styles.whoBtnWrapper}>
                   <div className={styles.whoBtn}>
-                    <input type="radio" id="company" name="company" />
+                    <input
+                      type="radio"
+                      id="company"
+                      name="who"
+                      value="company"
+                    />
                     <label htmlFor="company">Company</label>
                   </div>
                   <div className={styles.whoBtn}>
-                    <input type="radio" id="organization" name="organization" />
+                    <input
+                      type="radio"
+                      id="organization"
+                      name="who"
+                      value="organization"
+                    />
                     <label htmlFor="organization">Organization</label>
                   </div>
                   <div className={styles.whoBtn}>
-                    <input type="radio" id="individual" name="individual" />
+                    <input
+                      type="radio"
+                      id="individual"
+                      name="who"
+                      value="individual"
+                    />
                     <label htmlFor="individual">Individual</label>
                   </div>
-                </div>
+                </fieldset>
                 <div className={styles.whoFieldWrapper}>
                   <div className={styles.whoField}>
                     <input
