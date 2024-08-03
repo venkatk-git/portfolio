3aa280472b34c9d806037c330045b733bc9de11e
venkatk-git <venkatkumar.profiles@gmail.com>
2024-08-03 17:46:18 +0530

    Initialized

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -21,10 +21,33 @@ function ProjectCard({ isActive, cardToggleHandler, ...deleagated }) {
         ease: "easeIn",
       }}
     >
-      <div className={styles.cardWrapper}></div>
+      <div className={styles.cardWrapper}>
+        <div className={styles.cardLeft}></div>
+        {isActive && (
+          <div className={styles.cardRight}>
+            <div className={styles.projectContext}>
+              <p>
+                This Campus Management System is a full-stack application
+                developed to address various challenges faced by the
+                college&apos;s Campus Management Team. The team needed an
+                efficient solution for maintaining logs, assigning tasks,
+                tracking work status, and managing the workforce, including
+                workers and supervisors. Leveraging my web development skills, I
+                created a comprehensive dashboard that streamlines these
+                processes. The project was built using the MEVN stack (MongoDB,
+                Express.js, Vue.js, Node.js). Although I was initially
+                unfamiliar with Vue.js, I quickly learned and applied it to
+                deliver a robust and user-friendly system that meets the
+                team&apos;s requirements. This project not only helped solve a
+                real-world problem but also expanded my expertise in full-stack
+                development and Vue.js.
+              </p>
+            </div>
+          </div>
+        )}
+      </div>
     </motion.div>
   );
 }
 
 export default ProjectCard;
-  
