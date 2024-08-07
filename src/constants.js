925a37e4d59a9d9d090f498e34565211792b9b1a
venkatk-git <venkatkumar.profiles@gmail.com>
2024-08-07 19:14:30 +0530

    Added Projects Images

--- src/constants.js
+++ src/constants.js
@@ -25,7 +25,7 @@ export const PROJECTS = [
     title: "Campus Management System",
     description:
       "This Campus Management System is a full-stack application developed to address various challenges faced by the college's Campus Management Team. The team needed an efficient solution for maintaining logs, assigning tasks, tracking work status, and managing the workforce, including workers and supervisors.",
-    link: "#",
+    link: "https://github.com/venkatk-git/Campus-Management-Portal.git",
     img: "https://raw.githubusercontent.com/venkatk-git/portfolio/projects-card-02/src/docs/project_01_01.png",
     stack: (
       <>
