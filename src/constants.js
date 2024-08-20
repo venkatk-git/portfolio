3ccd97e28c1a5189a623b3f7d637023a59505e3e
venkatk-git <venkatkumar.profiles@gmail.com>
2024-08-20 18:08:33 +0530

    Removed Projects

--- src/constants.js
+++ src/constants.js
@@ -35,32 +35,4 @@ export const PROJECTS = [
       </>
     ),
   },
-  {
-    title: "Campus Management System",
-    description:
-      "This Campus Management System is a full-stack application developed to address various challenges faced by the college's Campus Management Team. The team needed an efficient solution for maintaining logs, assigning tasks, tracking work status, and managing the workforce, including workers and supervisors.",
-    link: "https://github.com/venkatk-git/Campus-Management-Portal.git",
-    img: "https://github.com/venkatk-git/portfolio/blob/dev/src/docs/campus_management_system.png?raw=true",
-    stack: (
-      <>
-        <Icon icon="skill-icons:mongodb" />
-        <Icon icon="skill-icons:vuejs-dark" />
-        <Icon icon="skill-icons:nodejs-dark" />
-      </>
-    ),
-  },
-  {
-    title: "Campus Management System",
-    description:
-      "This Campus Management System is a full-stack application developed to address various challenges faced by the college's Campus Management Team. The team needed an efficient solution for maintaining logs, assigning tasks, tracking work status, and managing the workforce, including workers and supervisors.",
-    link: "https://github.com/venkatk-git/Campus-Management-Portal.git",
-    img: "https://github.com/venkatk-git/portfolio/blob/dev/src/docs/campus_management_system.png?raw=true",
-    stack: (
-      <>
-        <Icon icon="skill-icons:mongodb" />
-        <Icon icon="skill-icons:vuejs-dark" />
-        <Icon icon="skill-icons:nodejs-dark" />
-      </>
-    ),
-  },
 ];
