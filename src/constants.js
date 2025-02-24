86f7d76078c924dd7562d933a9e90748b9a013a2
venkatk-git <venkatkumar.profiles@gmail.com>
2025-02-24 23:17:33 +0530

    add projects

--- src/constants.js
+++ src/constants.js
@@ -22,6 +22,20 @@ export const LINKS = {
 import { Icon } from "@iconify/react";
 export const PROJECTS = [
     {
+        title: "Stream",
+        description:
+            "Stream is a platfrom to watch youtube videos together with friends. It is a full-stack application developed using MERN stack. It uses socket.io for real-time communication between users.",
+        link: "https://github.com/venkatk-git/stream",
+        img: "https://github.com/venkatk-git/portfolio/blob/main/public/assests/stream.png?raw=true",
+        stack: (
+            <>
+                <Icon icon="skill-icons:mongodb" />
+                <Icon icon="skill-icons:reactjs-dark" />
+                <Icon icon="skill-icons:nodejs-dark" />
+            </>
+        ),
+    },
+    {
         title: "Campus Management System",
         description:
             "This Campus Management System is a full-stack application developed to address various challenges faced by the college's Campus Management Team. The team needed an efficient solution for maintaining logs, assigning tasks, tracking work status, and managing the workforce, including workers and supervisors.",
@@ -31,20 +45,6 @@ export const PROJECTS = [
             <>
                 <Icon icon="skill-icons:mongodb" />
                 <Icon icon="skill-icons:vuejs-dark" />
-                <Icon icon="skill-icons:nodejs-dark" />
-            </>
-        ),
-    },
-    {
-        title: "Stream",
-        description:
-            "Stream is a platfrom to watch youtube videos together with friends. It is a full-stack application developed using MERN stack. It uses socket.io for real-time communication between users.",
-        link: "https://github.com/venkatk-git/stream",
-        img: "https://github.com/venkatk-git/portfolio/blob/dev/src/docs/campus_management_system.png?raw=true",
-        stack: (
-            <>
-                <Icon icon="skill-icons:mongodb" />
-                <Icon icon="skill-icons:reactjs-dark" />
                 <Icon icon="skill-icons:nodejs-dark" />
             </>
         ),
